f8bbe0115b8fad79c12cb8b4abd06bcc0f348190
Timothy Lee <timothy.ty.lee@gmail.com>
2025-06-07 14:48:10 +1000

    implement osDelay()

---
+++ src/cmsisPosix_Delay.c
@@ -0,0 +1,26 @@
+#define _GNU_SOURCE
+#include <errno.h>
+
+#include <stdint.h>
+#include <time.h>
+#include "cmsis_os2.h"
+#include "cmsisPosix_Config.h"
+
+// Forward declarations
+void cp_timeoutToTimespec(uint32_t timeout, struct timespec *ts);
+
+osStatus_t osDelay(uint32_t ticks)
+{
+    struct timespec ts;
+    cp_timeoutToTimespec(ticks, &ts);
+
+    // Sleep until it is time
+    while (1) {
+        int posix_ret =  clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &ts, NULL);
+        if (posix_ret == 0) {
+            return osOK;
+        } else if (posix_ret != EINTR) {
+            return osError;
+        }
+    }
+}
