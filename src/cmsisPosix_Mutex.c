997ae576873a22febab6b82c2f320857c4d38f5e
amgross <aryespa@gmail.com>
2025-06-10 22:20:51 +0300

    Implemented osMutexGetOwner

--- src/cmsisPosix_Mutex.c
+++ src/cmsisPosix_Mutex.c
@@ -14,65 +14,80 @@
 typedef struct {
     pthread_mutex_t mutex;
     const char *name;
+    osThreadId_t *owner;
 } cmsisPosix_mutexHandler_t;
 
 osMutexId_t osMutexNew(const osMutexAttr_t *attr)
 {
     // Allocate memory for our mutex wrapper
-    cmsisPosix_mutexHandler_t *mutex;
+    cmsisPosix_mutexHandler_t *mutex = NULL;
 
     // Initialize the mutex attributes
     pthread_mutexattr_t mutex_attr;
-    if (pthread_mutexattr_init(&mutex_attr) != 0) {
+    if (pthread_mutexattr_init(&mutex_attr) != 0)
+    {
         return NULL;
     }
 
     mutex = (cmsisPosix_mutexHandler_t *)malloc(sizeof(cmsisPosix_mutexHandler_t));
-    if (mutex == NULL) {
-        return NULL;  // Memory allocation failed
+    if (mutex == NULL)
+    {
+        goto error_exit;
     }
 
 
     // Set up mutex according to CMSIS attributes if provided
-    if (attr != NULL) {
+    if (attr != NULL)
+    {
         // Set name provided (If NULL then NULL)
         mutex->name = attr->name;
-    } else {
+
+        //TODO deal with all other attributes
+    }
+    else
+    {
         // Default attributes
         mutex->name = NULL;
     }
-
+    
     // Initialize the mutex with attributes
-    if (pthread_mutex_init(&mutex->mutex, &mutex_attr) != 0) {
-        pthread_mutexattr_destroy(&mutex_attr);
-        free(mutex);
-        return NULL;
+    if (pthread_mutex_init(&mutex->mutex, &mutex_attr) != 0)
+    {
+        goto error_exit;
     }
 
     // Clean up the attributes
     pthread_mutexattr_destroy(&mutex_attr);
 
     return (osMutexId_t)mutex;
+
+error_exit:
+    pthread_mutexattr_destroy(&mutex_attr);
+    free(mutex);
+    return NULL;
 }
 
 osStatus_t osMutexAcquire(osMutexId_t mutex_id, uint32_t timeout)
 {
     cmsisPosix_mutexHandler_t *mutex = (cmsisPosix_mutexHandler_t *)mutex_id;
     int posix_ret;
+    osThreadId_t curr_thread;
     
     if (mutex == NULL)
     {
         return osErrorParameter;
     }
-    
+
+    curr_thread = osThreadGetId();
+    if(curr_thread == NULL)
+    {
+        return osErrorResource;
+    }
+
     if (timeout == 0)
     {
         // Try to lock without waiting
-        if (pthread_mutex_trylock(&mutex->mutex) == 0)
-        {
-            return osOK;
-        }
-        else
+        if (pthread_mutex_trylock(&mutex->mutex) != 0)
         {
             return osErrorResource;
         }
@@ -80,11 +95,7 @@ osStatus_t osMutexAcquire(osMutexId_t mutex_id, uint32_t timeout)
     else if (timeout == osWaitForever)
     {
         // Block indefinitely
-        if (pthread_mutex_lock(&mutex->mutex) == 0)
-        {
-            return osOK;
-        }
-        else
+        if (pthread_mutex_lock(&mutex->mutex) != 0)
         {
             return osErrorResource;
         }
@@ -95,31 +106,37 @@ osStatus_t osMutexAcquire(osMutexId_t mutex_id, uint32_t timeout)
         cp_timeoutToTimespec(timeout, &ts);
 
         posix_ret = pthread_mutex_timedlock(&mutex->mutex, &ts);
-        if(0 == posix_ret)
+        if(0 != posix_ret)
         {
-            return osOK;
-        }
-        else if(ETIMEDOUT == posix_ret)
-        {
-            return osErrorTimeout;
-        }
-        else
-        {
-            return osErrorResource;
+            if(ETIMEDOUT == posix_ret)
+            {
+                return osErrorTimeout;
+            }
+            else
+            {
+                return osErrorResource;
+            }
         }
     }
+
+    mutex->owner = curr_thread;
+
+    return osOK;
 }
 
 osStatus_t osMutexRelease(osMutexId_t mutex_id)
 {
     cmsisPosix_mutexHandler_t *mutex = (cmsisPosix_mutexHandler_t *)mutex_id;
     
-    if (mutex == NULL) {
+    if (mutex == NULL)
+    {
         return osErrorParameter;
     }
-    
+
     if (pthread_mutex_unlock(&mutex->mutex) == 0)
     {
+        // No need to ensure current thread is the owner before releasing, this is handled by POSIX
+        mutex->owner = NULL;
         return osOK;
     }
     else
@@ -128,7 +145,20 @@ osStatus_t osMutexRelease(osMutexId_t mutex_id)
     }
 }
 
-osStatus_t osMutexDelete(osMutexId_t mutex_id) {
+osThreadId_t osMutexGetOwner (osMutexId_t mutex_id)
+{
+    cmsisPosix_mutexHandler_t *mutex = (cmsisPosix_mutexHandler_t *)mutex_id;
+
+    if(mutex == NULL)
+    {
+        return NULL;
+    }
+
+    return mutex->owner;
+}
+
+osStatus_t osMutexDelete(osMutexId_t mutex_id)
+{
     cmsisPosix_mutexHandler_t *mutex = (cmsisPosix_mutexHandler_t *)mutex_id;
     
     if (mutex == NULL)
---
+++ tests/config1/src/mutex_get_owner.c
@@ -0,0 +1,75 @@
+#include "cmsis_os2.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include "cmsisPosix_TestInfra.h"
+
+void Thread_owner(void *argument);
+void second_thread(void *argument);
+
+osThreadId_t thread_id;
+static osMutexId_t mutex;
+
+void test_start(void)
+{
+    osThreadId_t mutex_owner;
+
+    osKernelInitialize();
+
+    mutex = osMutexNew(NULL);          // Create mutex with default attributes
+    CP_ASSERT_NE(mutex, NULL);
+
+    // Ensure no owner before aquire
+    mutex_owner = osMutexGetOwner(mutex);
+    CP_ASSERT_EQ(mutex_owner, NULL);
+
+    thread_id = osThreadNew(Thread_owner, NULL, NULL);
+    CP_ASSERT_NE(Thread_owner, NULL);
+
+    osKernelStart();
+
+    CP_UASSERT_NREACHABLE();
+}
+
+void Thread_owner(void *argument)
+{
+    (void)argument;
+
+    osStatus_t status;
+    osThreadId_t curr_thread_id, mutex_owner, second_thread_id;
+
+    status = osMutexAcquire(mutex, osWaitForever);
+    CP_ASSERT_EQ(status, osOK);
+
+    curr_thread_id = osThreadGetId();
+    CP_ASSERT_NE(curr_thread_id, NULL);
+    CP_ASSERT_EQ(curr_thread_id, thread_id);
+    mutex_owner = osMutexGetOwner(mutex);
+    CP_ASSERT_NE(mutex_owner, NULL);
+
+    CP_ASSERT_EQ(thread_id, mutex_owner);
+
+    second_thread_id = osThreadNew(second_thread, NULL, NULL);
+    CP_ASSERT_NE(second_thread_id, NULL);
+
+    osDelay(20);
+
+    status = osMutexRelease(mutex);
+    CP_ASSERT_EQ(status, osOK);
+
+    // Ensure no owner after release
+    mutex_owner = osMutexGetOwner(mutex);
+    CP_ASSERT_EQ(mutex_owner, NULL);
+    
+}
+
+void second_thread(void *argument)
+{
+    (void)argument;
+
+    osThreadId_t mutex_owner;
+
+    mutex_owner = osMutexGetOwner(mutex);
+    CP_ASSERT_NE(mutex_owner, NULL);
+
+    CP_ASSERT_EQ(thread_id, mutex_owner);
+}
